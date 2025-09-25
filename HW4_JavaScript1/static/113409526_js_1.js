9b15ca9c4748e1af6083eafe45df81e8f12002d2
leotsai940914 <qqq113579@gmail.com>
2025-09-25 15:25:55 +0800

    作業一完成

--- HW4_JavaScript1/static/113409526_js_1.js
+++ HW4_JavaScript1/static/113409526_js_1.js
@@ -27,7 +27,14 @@
         triesEl.textContent = String(tries); //更新畫面上的輸入次數
 
         if (val === answer) {
-            alert(`恭喜你答對了!你共猜了 ${tries} 次`); //答對就跳出恭喜視窗
+            alert(`恭喜你答對了!你共猜了 ${tries} 次 \n下一題，繼續挑戰!`); //答對就跳出恭喜視窗
+            //重設遊戲
+            answer = Math.floor(Math.random() * 101); //產生新答案
+            tries = 0; //重設輸入次數
+            triesEl.textContent = "0"; //更新畫面上的輸入次數
+            input.value = ""; //清空輸入框
+            console.log("答案", answer); //印出新答案在 console
+            
         }   else if (val < answer) {
             alert("太小了!"); //比答案小就跳出太小視窗
         }   else {
