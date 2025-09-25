0d2790934eb3109e9e6669b993528da1b5120be4
leotsai940914 <qqq113579@gmail.com>
2025-09-25 14:41:49 +0800

    完成在console印出輸入的數字 解決html沒有加defer的錯誤

--- HW4_JavaScript1/static/113409526_js_1.js
+++ HW4_JavaScript1/static/113409526_js_1.js
@@ -1,12 +1,23 @@
 (() => {
     "use strict"; //嚴格模式，不知道什麼意思
+
+    const $ = (sel) => document.querySelector(sel); //小工具，幫忙選取元素
+    const form = $("#guess-form"); 
+    const input = $("#guess");
     
-    //產生 0 到 100 的整數
-    let answer = Math.floor(Math.random() * 101);
+    let answer = Math.floor(Math.random() * 101);//產生 0 到 100 的整數
+    console.log("答案", answer); //先印出答案在 console
 
-    //記住輸入的次數
-    let tries = 0;
+    //監聽表單送出事件
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); //防止表單送出後頁面重新整理
 
-    //先印出答案在 console
-    console.log("答案", answer);
+        const val = input.value; //取得輸入的數字
+        console.log("你輸入的是", val); //印出輸入的數字在 console
+    });
+
+    let tries = 0; //記住輸入的次數
+
+   
+   
 })();
