f80ef54d7dc41024c1742c2a9654d42d5b25d574
leotsai940914 <qqq113579@gmail.com>
2025-09-25 14:16:53 +0800

    可以產生隨機數字和顯示於console

--- HW4_JavaScript1/static/113409526_js_1.js
+++ HW4_JavaScript1/static/113409526_js_1.js
@@ -1,1 +1,12 @@
-//產生 0 到 100 的整數
+(() => {
+    "use strict"; //嚴格模式，不知道什麼意思
+    
+    //產生 0 到 100 的整數
+    let answer = Math.floor(Math.random() * 101);
+
+    //記住輸入的次數
+    let tries = 0;
+
+    //先印出答案在 console
+    console.log("答案", answer);
+})();
