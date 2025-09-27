64db3ec972ba1ad172decad44462bf43ccb16936
leotsai940914 <qqq113579@gmail.com>
2025-09-27 11:27:04 +0800

    迴圈生成數字按鈕

--- HW4_JavaScript1/static/113409526_js_2.js
+++ HW4_JavaScript1/static/113409526_js_2.js
@@ -3,7 +3,22 @@
 
     document.write(`
         <div style="width:220px; margin:20px auto;">
-            <input id="display" type="text" readonly style="width:100%; height:40px; font-size:18px;"/>
+            <input id="display" type="text" readonly 
+                style="width:100%; height:40px; font-size:18px;"/>
+            <div id="buttons"></div>
         </div>
     `);
+
+    //迴圈生成0-9按鈕
+    for (let i = 0; i <=9; i++) {
+        document.write(`
+            <button class="btn" data-val="${i}">${i}</button>`);
+        if (i % 3 === 0 && i !== 0) {
+            document.write('<br/>');
+            //每三個按鈕換行
+            }   
+    }
+
+    
+    
 })();
