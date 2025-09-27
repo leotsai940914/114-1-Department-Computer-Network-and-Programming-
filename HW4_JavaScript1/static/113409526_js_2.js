c8a76aeb52f83fd17ca18a7902322aee0af07791
leotsai940914 <qqq113579@gmail.com>
2025-09-27 10:16:07 +0800

    初步架構

---
+++ HW4_JavaScript1/static/113409526_js_2.js
@@ -0,0 +1,9 @@
+(() => {
+    "use strict";
+
+    document.write(`
+        <div style="width:220px; margin:20px auto;">
+            <input id="display" type="text" readonly style="width:100%; height:40px; font-size:18px;"/>
+        </div>
+    `);
+})();
