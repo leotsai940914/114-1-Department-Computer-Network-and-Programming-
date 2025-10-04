ae5b4824a89ab47c8e4ee6c218c26d7785a548ae
leotsai940914 <qqq113579@gmail.com>
2025-10-04 15:07:15 +0800

    計算總價值

--- HW5_JavaScript2/static/113409526_js_2(購物車).js
+++ HW5_JavaScript2/static/113409526_js_2(購物車).js
@@ -7,6 +7,28 @@
   const master = $("#checkbox_all");
   const itemCheckboxes = $$(".item_checkbox");
 
-  //
+
+  //calculate the total price
+  function calcTotal(){
+    let total = 0;
+    $$(".item-row").forEach((row) => {
+      const price = Number(row.querySelector(".item-price").textContent);
+      const qty = Number(row.querySelector(".item-qty").value);
+      const subtotal = price * qty;
+      row.querySelector(".item-checkbox").textContent = subtotal;
+
+
+      //only selected number would be calculated into total
+      if (row.querySelector(".item-checkbox").checked) {
+        total += subtotal;
+      }
+    });
+    $("#total").textContent = total;
+  }
+
+  //master checkbox control all item checkboxes
+  
+
+
 
 }
