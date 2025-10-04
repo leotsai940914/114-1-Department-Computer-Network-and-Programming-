6912f4b3fba9caa68b6bcfe18e0b6c9108b142d5
leotsai940914 <qqq113579@gmail.com>
2025-10-04 11:41:08 +0800

    定義了四個涵式來簡化

--- HW5_JavaScript2/static/113409526_js_2(購物車).js
+++ HW5_JavaScript2/static/113409526_js_2(購物車).js
@@ -1,1 +1,12 @@
+(() => {
+  "use strict";
 
+  const $ = (sel) => document.querySelector(sel);
+  const $$ = (sel) => Array.from(document.querySelectorAll(sel));
+
+  const master = $("#checkbox_all");
+  const itemCheckboxes = $$(".item_checkbox");
+
+  //
+
+}
