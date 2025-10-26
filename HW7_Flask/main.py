90327086df381b842825fcb9a961375fff435601
蔡晟郁 <tsaichengyu@caichengyudeMacBook-Air.local>
2025-10-26 11:22:14 +0800

    connect to sqlite

--- HW7_Flask/main.py
+++ HW7_Flask/main.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
+app.secret_key = 'your_very_secret_key_12345'
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+        username_from_form = request.form['username']
+        password_from_form = request.form['password']
+
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+
+        query = "SELECT * FROM teachers WHERE username = ? AND password = ?"
+
+        c.execute(query, (username_from_form, password_from_form))
+        teacher = c.fetchone
+
+        conn.close()
+
 def show_login_page():
     return render_template('login.html')
 
