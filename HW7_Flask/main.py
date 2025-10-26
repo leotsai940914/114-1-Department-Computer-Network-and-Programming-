19e0e30c7e6f9e783ceb13028c3985fa64061234
蔡晟郁 <tsaichengyu@caichengyudeMacBook-Air.local>
2025-10-26 11:30:56 +0800

    change the route

--- HW7_Flask/main.py
+++ HW7_Flask/main.py
@@ -9,19 +9,27 @@ app.secret_key = 'your_very_secret_key_12345'
 def login():
     error = None
 
+    # 檢查請求方法
     if request.method == 'POST':
         username_from_form = request.form['username']
         password_from_form = request.form['password']
-
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
-
         query = "SELECT * FROM teachers WHERE username = ? AND password = ?"
-
         c.execute(query, (username_from_form, password_from_form))
         teacher = c.fetchone
-
         conn.close()
+
+        if teacher:
+            session['username'] = username_from_form
+            return redirect(url_for('grades'))
+        
+        else:
+            error = "錯誤的名稱或錯誤的密碼"
+
+        
+    return render_template('login.html', error=error)
+
 
 def show_login_page():
     return render_template('login.html')
