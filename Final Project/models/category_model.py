82b2b73339169df1a20ff0cdd2877b670de0b2a8
蔡晟郁 <tsaichengyu@caichengyudeMacBook-Air.local>
2025-12-10 12:20:19 +0800

    Complete Step 5-3: Category page loads posts from DB

--- Final Project/models/category_model.py
+++ Final Project/models/category_model.py
@@ -1,6 +1,12 @@
 from models.database import get_db_connection
 
 class CategoryModel:
+    @staticmethod
+    def get_category_by_name(name):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM categories WHERE name = ?", (name,))
+        return cursor.fetchone()
 
     @staticmethod
     def get_all_categories():
